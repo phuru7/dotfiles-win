915c20736e8ca3086995a57a033a9facd4432805
Santiago Pajuelo <santiago@phuru.io>
2022-05-31 20:42:23 -0500

    Update install_wsl.ps1

--- install_wsl.ps1
+++ install_wsl.ps1
@@ -2,4 +2,6 @@
 ## Activate WLS2
 dism.exe /online /Enable-Feature /Featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /Enable-Feature /FeatureName:VirtualMachinePlatform /all /norestart
-wsl --set-version Ubuntu 2
+
+wsl --list --verbose
+wsl --set-version <distro> 2
