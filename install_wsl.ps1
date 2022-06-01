075d81bb51e67576589ca35dedc98a7f501bd7f6
Santiago Pajuelo <santiago@phuru.io>
2022-05-31 20:45:15 -0500

    Update install_wsl.ps1

--- install_wsl.ps1
+++ install_wsl.ps1
@@ -2,6 +2,7 @@
 ## Activate WLS2
 dism.exe /online /Enable-Feature /Featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /Enable-Feature /FeatureName:VirtualMachinePlatform /all /norestart
+dism.exe /online /Enable-Feature /FeatureName:Microsoft-Hyper-V /all /norestart
 
 wsl --set-default-version 2
 wsl --list --verbose
